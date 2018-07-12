6550c87612349892624e9e309568ae38c801d9d9
Andrew Morton <morton09@me.com>
2018-07-11 19:41:08 -0500

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+import jenkins
+import devCreds
+
+server = jenkins.Jenkins(devCreds.jenkins_url, username=devCreds.username, password=devCreds.password)
+
+user = server.get_whoami()
+version = server.get_version()
+
+all_jobs = server.get_jobs()
+job_count = server.jobs_count()
+print(job_count)
---
+++ creds.py
@@ -0,0 +1,3 @@
+username='Dev01'
+password='DevTest'
+jenkins_url='http://localhost:8080'
